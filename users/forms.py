e0e6f320e074151016eae64499b408ca22ed7d65
Basir Tibash <abdolbasir.tibash@gmail.com>
2025-08-17 11:35:52 +0330

    feat: Add login page to the main project
    Create a login form
    create a user_login view
    create a simple login.html template
    Add a new url as login to the app

--- users/forms.py
+++ users/forms.py
@@ -1,3 +1,7 @@
 from django import forms
 
 # Create your forms here.
+class LoginForm(forms.Form):
+    username = forms.CharField()
+    password = forms.CharField(widget=forms.PasswordInput)
+    
--- users/views.py
+++ users/views.py
@@ -1,3 +1,25 @@
 from django.shortcuts import render
-
+from django.http import HttpResponse
+from django.contrib.auth import authenticate, login
+from .forms import LoginForm
 # Create your views here.
+
+def user_login(request):
+    if request.method == "POST":
+        form = LoginForm(request.POST)
+        if form.is_valid():
+            data = form.cleaned_data
+            user = authenticate(
+                        request, 
+                        username=data['username'],
+                        password = data['password']
+                        )
+            print(user)
+            if user is not None:
+                login(request, user)
+                return HttpResponse("User authenticated and logged in")
+            else:
+                return HttpResponse("Invalid Username or password")
+    else:
+        form = LoginForm()
+    return render(request, 'users/login.html', {'form': form})
