8bdbd5aa81270bca08382ff572bcb522bc6c1dd2
Basir Tibash <abdolbasir.tibash@gmail.com>
2025-08-17 12:31:35 +0330

    feat: Add logout to the app
    Add a base.html to the project
    Add logout.html
    Change urls.py in user app

--- users/urls.py
+++ users/urls.py
@@ -1,5 +1,7 @@
 from django.urls import path
+from django.contrib.auth.views import LogoutView
 from . import views
 urlpatterns = [
-    path('login/', views.user_login, name='login')
+    path('login/', views.user_login, name='login'),
+    path('logout/', LogoutView.as_view(template_name="users/logout.html"), name='logout')     
 ]
