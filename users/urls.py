6727283152442db8aca57239edb1043e9fcdd23b
Basir Tibash <abdolbasir.tibash@gmail.com>
2025-08-18 15:34:00 +0330

    feat: Add index(users) to app
    Add index.html to the templates
    Add index view to the views
    Add users url to the urls
    Add defualt login and logut url to the settings

--- users/urls.py
+++ users/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 from django.contrib.auth.views import LogoutView
 from . import views
 urlpatterns = [
+    path('', views.index, name='index'),
     path('login/', views.user_login, name='login'),
     path('logout/', LogoutView.as_view(template_name="users/logout.html"), name='logout')     
 ]
--- users/views.py
+++ users/views.py
@@ -1,8 +1,13 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.contrib.auth import authenticate, login
+from django.contrib.auth.decorators import login_required
 from .forms import LoginForm
 # Create your views here.
+
+@login_required
+def index(request):
+    return render(request, 'users/index.html')
 
 def user_login(request):
     if request.method == "POST":
