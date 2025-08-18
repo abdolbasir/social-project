057b64ccfc8c2734b2a6e1e22a3f263babe113cd
Basir Tibash <abdolbasir.tibash@gmail.com>
2025-08-18 16:27:59 +0330

    feat: Add passowrd change to the app
    Add password_change.html and password_change_done.html pages
    Add path to this two templates in urls.py file

--- users/urls.py
+++ users/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
-from django.contrib.auth.views import LogoutView
+from django.contrib.auth.views import LogoutView, PasswordChangeView, PasswordChangeDoneView
 from . import views
 urlpatterns = [
     path('', views.index, name='index'),
     path('login/', views.user_login, name='login'),
-    path('logout/', LogoutView.as_view(template_name="users/logout.html"), name='logout')     
+    path('logout/', LogoutView.as_view(template_name="users/logout.html"), name='logout'),
+    path('password_change/', PasswordChangeView.as_view(template_name='users/password_change.html'), name='password_change'),
+    path('password_change/done', PasswordChangeDoneView.as_view(template_name='users/password_change_done.html'), name='password_change_done')  
 ]
